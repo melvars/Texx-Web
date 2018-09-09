86256606d2312204295f111a26ebbbe05d024014
Marvin Borner <marvin-borner@live.com>
2018-09-09 20:01:46 +0200

    Reformatted code and fixed bugs :zap: :art:

--- assets/js/preloader.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function(){
-    $("#ud_dots")
-        .fadeOut(300, function() {
-            $("#ud_bg_left").animate({ left: "-50%" }, 300);
-            $("#ud_bg_right").animate({ right: "-50%" }, 300, function() {
-                $("#ud_preloader").fadeOut(10);
-            });
-        });
-});
