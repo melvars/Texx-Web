eca7ce7e7e87a0f51167f19da193e79ad46fc0f5
Marvin Borner <marvin-borner@live.com>
2018-09-09 21:19:46 +0200

    Fixed animations :bug:

--- assets/js/preLoader.js
+++ assets/js/preLoader.js
@@ -1,6 +1,7 @@
 $(window).on("load", () => {
     $("#udDots")
         .fadeOut(300, () => {
+            $(".typewriter h1").fadeIn();
             $("#udBgLeft").animate({left: "-50%"}, 300);
             $("#udBgRight").animate({right: "-50%"}, 300, () => {
                 $("#udPreLoader").fadeOut(10);
