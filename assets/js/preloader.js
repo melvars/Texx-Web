eb6da3acc10cecd4d5ed757a8df60ebe1a9d7374
Marco Alexander Fischer <marco.a.fischer@t-online.de>
2018-09-09 19:50:42 +0200

    Added bugs :hankey:

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,6 +1,5 @@
-$(document).ready(function() {
+$(window).load(function(){
     $("#ud_dots")
-        .delay(750)
         .fadeOut(300, function() {
             $("#ud_bg_left").animate({ left: "-50%" }, 300);
             $("#ud_bg_right").animate({ right: "-50%" }, 300, function() {
